c153db3e0e148795ef31380e535634ec4d0fb20b
Josh Epstein <joshua.epstein@sky.uk>
2025-10-03 15:02:54 +0100

    feat: Initial commit \o/
    Signed-off-by: Epstein <jea025@uklpqvk4qvkw.localdomain>

---
+++ README.md
@@ -0,0 +1,8 @@
+# JSE Project 1
+
+Task 1:
+
+[ x ] Setup Java Project with JDK
+[ x ] Setup GitHub Repo
+[ x ] Push to github
+[ ] Create README explaining project (WIP as there is no code)
---
+++ src/joshuaepstein/Main.java
@@ -0,0 +1,10 @@
+package joshuaepstein;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO
+        System.out.println("Welcome to this amazing project :)");
+    }
+
+}
